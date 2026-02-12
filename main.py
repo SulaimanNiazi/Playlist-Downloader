a9001a7c9fbc5f5ea6701f6dd09989222b107f91
Muhammad Sulaiman <suleman.j2002@gmail.com>
2026-02-12 16:51:38 +0500

    Add Location Entry row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, Label, Entry, Button, StringVar, BooleanVar, Checkbutton
 from tkinter.ttk import Combobox
+from tkinter.filedialog import askdirectory
 
 class gui:
     def __init__(self, root: Tk):
@@ -17,6 +18,11 @@ class gui:
         self.url_bar.grid(column=1, columnspan=2, row=0, padx=10, sticky="we")
         Button(root, text="Download", command=self.download).grid(column=3, row=0, padx=10, sticky="we")
 
+        Label(root, text="Location:").grid(column=0, row=1, padx=10, sticky="we")
+        self.path = Entry(root)
+        self.path.grid(column=1, columnspan=2, row=1, padx=10, sticky="we")
+        Button(root, text="Browse", command=lambda: self.path.insert(0, askdirectory(initialdir="."))).grid(column=3, row=1, padx=10, sticky="we")
+
         Label(root, text="Options:").grid(column=0, row=2, padx=10, sticky="we")
         video_formats = ["No Video Downloads", "8K UHD 4320p", "4K UHD 2160p", "QHD 1440p", "FHD 1080p", "HD 720p", "SD 480p", "SD 360p", "LD 240p", "LD 144p"]
         self.video_option = StringVar(value=video_formats[0])
@@ -28,7 +34,10 @@ class gui:
         Checkbutton(root, text="Catalog", variable=self.catalog_option).grid(column=3, row=2, padx=10, sticky="we")
     
     def download(self):
-        pass
+        url = self.url_bar.get()
+        print(url)
+        if not url:
+            return
 
 if __name__ == "__main__":
     root = Tk()
