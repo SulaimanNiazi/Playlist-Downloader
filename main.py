18c597714d4efe12a30e89ca1c0bb805e576fb35
Muhammad Sulaiman <suleman.j2002@gmail.com>
2026-02-12 11:40:46 +0500

    Add size limits to window

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from tkinter import Tk
 class gui:
     def __init__(self, root: Tk):
         root.title("Media Downloader")
+        root.minsize(700, 400)
+        root.maxsize(root.winfo_screenwidth(), root.winfo_screenheight())
 
 if __name__ == "__main__":
     root = Tk()
