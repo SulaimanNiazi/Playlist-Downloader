f199af2d5626494ab697cd3e1d9b3176c89a1742
Muhammad Sulaiman <suleman.j2002@gmail.com>
2026-02-13 18:55:35 +0500

    Replace old options with format and quality comboboxes

--- main.py
+++ main.py
@@ -16,28 +16,37 @@ class gui:
         Label(root, text="URL:").grid(column=0, row=0, padx=10, sticky="we")
         self.url_bar = Entry(root)
         self.url_bar.grid(column=1, columnspan=2, row=0, padx=10, sticky="we")
-        Button(root, text="Download", command=self.download).grid(column=3, row=0, padx=10, sticky="we")
+        button = Button(root, text="Download", command=self.button_pressed)
+        button.grid(column=3, row=0, padx=10, sticky="we")
 
         Label(root, text="Location:").grid(column=0, row=1, padx=10, sticky="we")
         self.path = Entry(root)
         self.path.grid(column=1, columnspan=2, row=1, padx=10, sticky="we")
         Button(root, text="Browse", command=lambda: self.path.insert(0, askdirectory(initialdir="."))).grid(column=3, row=1, padx=10, sticky="we")
 
+        formats = ["No Downloads", "m4a", "mkv", "mp3", "mp4"]
+        qualities = ["8K UHD 4320p", "4K UHD 2160p", "QHD 1440p", "FHD 1080p", "HD 720p", "SD 480p", "SD 360p", "LD 240p", "LD 144p"]
+        
         Label(root, text="Options:").grid(column=0, row=2, padx=10, sticky="we")
-        video_formats = ["No Video Downloads", "8K UHD 4320p", "4K UHD 2160p", "QHD 1440p", "FHD 1080p", "HD 720p", "SD 480p", "SD 360p", "LD 240p", "LD 144p"]
-        self.video_option = StringVar(value=video_formats[0])
-        Combobox(root, state="readonly", textvariable=self.video_option, values=video_formats).grid(column=1, row=2, padx=10, sticky="we")
-        audio_formats = ["No Audio Downloads", "M4A"]
-        self.audio_option = StringVar(value=audio_formats[0])
-        Combobox(root, state="readonly", textvariable=self.audio_option, values=audio_formats).grid(column=2, row=2, padx=10, sticky="we")
+        self.format = StringVar(value=formats[0])
+        format_combo = Combobox(root, state="readonly", textvariable=self.format, values=formats)
+        format_combo.grid(column=1, row=2, padx=10, sticky="we")
+        self.quality = StringVar()
+        quality_combo = Combobox(root, state="readonly", textvariable=self.quality)
+        quality_combo.grid(column=2, row=2, padx=10, sticky="we")
         self.catalog_option = BooleanVar()
         Checkbutton(root, text="Catalog", variable=self.catalog_option).grid(column=3, row=2, padx=10, sticky="we")
-    
-    def download(self):
-        url = self.url_bar.get()
-        print(url)
-        if not url:
-            return
+        def set_qualities(event):
+            if self.format.get() in ("mkv", "mp4"):
+                self.quality.set(qualities[3])
+                quality_combo.config(values=qualities)
+            else:
+                self.quality.set("")
+                quality_combo.config(values=[])
+        format_combo.bind("<<ComboboxSelected>>", set_qualities)
+
+    def button_pressed(self):
+        pass
 
 if __name__ == "__main__":
     root = Tk()
